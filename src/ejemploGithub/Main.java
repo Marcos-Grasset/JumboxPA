610bbfe653d6d56ddf38687f1e8275a84363df1d
Marcos-Grasset <marcos.grasset@davinci.edu.ar>
2024-05-09 23:07:09 -0300

    Saludo

--- src/ejemploGithub/Main.java
+++ src/ejemploGithub/Main.java
@@ -1,10 +1,13 @@
 package ejemploGithub;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		JOptionPane.showMessageDialog(null, "Hola");
+		
 	}
-
 }
